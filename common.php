dc7dce18ee1c60350ae742cc8c8d3f0a0d960ab8
hobbyman <lance.perry.junk@gmail.com>
2019-05-06 19:19:35 -0500

    Update common.php

--- common.php
+++ common.php
@@ -1,43 +1,43 @@
 <?php
 
-define( "PICTORIAL_UID" , "dats_temp" );
-define( "PICTORIAL_PWD" , "br549" );
+define( "PICTORIAL_UID" , "uid" );
+define( "PICTORIAL_PWD" , "pwd" );
 
 if (
-    $_SERVER["SERVER_NAME"]=="alpha.in.dynetics.com"
-    || strstr(getcwd(),"alpha.in.dynetics.com")!=FALSE
-    || $_SERVER["SERVER_NAME"]=="web-dev1.in.dynetics.com"
-    || strstr(getcwd(),"web-dev1.in.dynetics.com")!=FALSE
+    $_SERVER["SERVER_NAME"]=="internal-dev1"
+    || strstr(getcwd(),"internal-dev1")!=FALSE
+    || $_SERVER["SERVER_NAME"]=="internal-dev2"
+    || strstr(getcwd(),"internal-dev2")!=FALSE
    ) {
 
     define( "HTTP_SERVER"  , "http://{$_SERVER["SERVER_NAME"]}" );
 
-    define( "PICTORIAL_HOST", "10.1.18.54" );
-    define( "PICTORIAL_DB"  , "dats_temp_dev_jan_2019" );
+    define( "PICTORIAL_HOST", "internal-dev" );
+    define( "PICTORIAL_DB"  , "tempdb" );
 
     $debug = true;
     define( "DEBUG", true );
-    define( "INCLUDES"    , "/home/perry/DEV_PhpIncludes/" );
+    define( "INCLUDES"    , "/home/uid/DEV_PhpIncludes/" );
 
-} else if ( $_SERVER["SERVER_NAME"]=="et.in.dynetics.com" || $_SERVER["SERVER_NAME"]=="ea.in.dynetics.com" ) {
+} else if ( $_SERVER["SERVER_NAME"]=="internal-prod" || $_SERVER["SERVER_NAME"]=="internal-prod" ) {
 
     define( "HTTP_SERVER"  , "http://{$_SERVER["SERVER_NAME"]}" );
 
-    define( "PICTORIAL_HOST", "aether" );
-    define( "PICTORIAL_DB"  , "dats_temp" );
+    define( "PICTORIAL_HOST", "internal-prod" );
+    define( "PICTORIAL_DB"  , "realdb" );
 
-    define( "INCLUDES"    , "/home/perry/PhpIncludes/" );
+    define( "INCLUDES"    , "/home/uid/PhpIncludes/" );
     define( "DEBUG", false );
 }
 
 
-require_once( "/home/perry/PhpIncludes/phpabstract.lib.php");
+require_once( "/home/uid/PhpIncludes/phpabstract.lib.php");
 $db   = new Db("pgsql");
 $conn = $db->db_connect( PICTORIAL_HOST, PICTORIAL_UID, PICTORIAL_PWD, PICTORIAL_DB);
 
-$rUser = str_replace("@in.dynetics.com","",strtolower($_SERVER["REMOTE_USER"]));
+$rUser = str_replace("@internal.domain.com","",strtolower($_SERVER["REMOTE_USER"]));
 $allowed = array(
-    'perry',
-    'baskin',
-    'hughesm'
+    'uid1',
+    'uid2',
+    'uid3'
 );
